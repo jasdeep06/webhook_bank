c2e559322bb0d07a213ca7092aec658c3caa9af0
Jasdeep Singh Chhabra <jasdeepchhabra94@gmail.com>
2017-09-04 19:21:41 +0530

    Add files via upload

---
+++ webhook.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_pymongo import PyMongo
+from flask import request
+import json
+import os
+app=Flask(__name__)
+
+app.config['MONGO_DBNAME']='bankfaq'
+app.config["MONGO_URI"]="mongodb://admin:admin@ds111204.mlab.com:11204/bankfaq"
+mongo=PyMongo(app)
+
+@app.route('/webhook',methods=['POST'])
+def webhook():
+    req=request.get_json(silent=True,force=True)
+    print('Request:',json.dumps(req,indent=4))
+    context=req.get("contexts").get("name")
+    result=req.get("result")
+    parameters=result.get("parameters")
+    objective=parameters.get("objective")
+    credit_card_faqs=mongo.db["credit_card_faqs"]
+    credit_card_faqs.create_index(['question','text'])
+    response=credit_card_faqs.find_one({"$text":{"$search":objective}})
+    return response['answer']
+
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print("Starting app on port %d" % port)
+
+    app.run(debug=False, port=port, host='0.0.0.0')
+
+
