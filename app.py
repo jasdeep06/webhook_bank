8318165583ee4a583e7a5b0507b637388becb500
Jasdeep Singh Chhabra <jasdeepchhabra94@gmail.com>
2017-09-04 20:36:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,8 +20,11 @@ def webhook():
     credit_card_faqs=mongo.db["credit_card_faqs"]
     credit_card_faqs.create_index([('question','text')])
     response=credit_card_faqs.find_one({"$text":{"$search":objective[0]}})
-    return response['answer']
-
+    res={displayText:response['answer']}
+    res=json.dumps(res,indent=4)
+    r=make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
 
 
 if __name__ == '__main__':
